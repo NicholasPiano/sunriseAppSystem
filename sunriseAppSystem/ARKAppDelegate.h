66cc844c0c4646806d5c968e0fd91bf71dcd866b
NicholasPiano <nicholas.d.piano@gmail.com>
2014-03-29 13:32:37 +0000

    Initial commit from local

---
+++ sunriseAppSystem/ARKAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  ARKAppDelegate.h
+//  sunriseAppSystem
+//
+//  Created by Nicholas Piano on 29/03/2014.
+//  Copyright (c) 2014 Nicholas Piano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ sunriseAppSystem/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  sunriseAppSystem
+//
+//  Created by Nicholas Piano on 29/03/2014.
+//  Copyright (c) 2014 Nicholas Piano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ARKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ARKAppDelegate class]));
+    }
+}
