4f0189743632413f1f14c0f47b488075f03d629a
NicholasPiano <nicholas.d.piano@gmail.com>
2014-04-05 02:36:27 +0100

    Adding state class.

---
+++ sunriseAppSystem/ARKState.h
@@ -0,0 +1,13 @@
+//
+//  ARKState.h
+//  sunriseAppSystem
+//
+//  Created by Nicholas Piano on 05/04/2014.
+//  Copyright (c) 2014 Nicholas Piano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ARKState : NSObject
+
+@end
---
+++ sunriseAppSystem/ARKState.m
@@ -0,0 +1,13 @@
+//
+//  ARKState.m
+//  sunriseAppSystem
+//
+//  Created by Nicholas Piano on 05/04/2014.
+//  Copyright (c) 2014 Nicholas Piano. All rights reserved.
+//
+
+#import "ARKState.h"
+
+@implementation ARKState
+
+@end
