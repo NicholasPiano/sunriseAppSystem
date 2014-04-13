e3f985ee7d5060786d1e1acd4c595cb14e2920a9
NicholasPiano <nicholas.d.piano@gmail.com>
2014-04-14 00:56:05 +0100

    Adding defaults class to supplement #definitions. Need class for method definitions.

---
+++ sunriseAppSystem/ARKDefault.h
@@ -0,0 +1,13 @@
+//
+//  ARKDefault.h
+//  sunriseAppSystem
+//
+//  Created by Nicholas Piano on 14/04/2014.
+//  Copyright (c) 2014 Nicholas Piano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ARKDefault : NSObject
+
+@end
---
+++ sunriseAppSystem/ARKDefault.m
@@ -0,0 +1,13 @@
+//
+//  ARKDefault.m
+//  sunriseAppSystem
+//
+//  Created by Nicholas Piano on 14/04/2014.
+//  Copyright (c) 2014 Nicholas Piano. All rights reserved.
+//
+
+#import "ARKDefault.h"
+
+@implementation ARKDefault
+
+@end
