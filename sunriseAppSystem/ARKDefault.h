22268d7e5b3949a52b0480e1311142302759aa0b
NicholasPiano <nicholas.d.piano@gmail.com>
2014-04-14 01:05:52 +0100

    Adding definitions. Commented values need to be added to prefix instead of having their own static methods.

--- sunriseAppSystem/ARKDefault.h
+++ sunriseAppSystem/ARKDefault.h
@@ -10,4 +10,65 @@
 
 @interface ARKDefault : NSObject
 
+#pragma mark initialize
++ (void)initialize; //setup all static vars
+
+#pragma mark fetch methods
+//system metrics
++ (CGSize)screenSize;
++ (CGPoint)centerScreen;
++ (CGPoint)centerScreenVerticalWithHorizontal:(CGFloat)horizontal;
++ (CGPoint)centerScreenHorizontalWithVertical:(CGFloat)vertical;
++ (CGFloat)screenHeight;
++ (CGFloat)screenWidth;
+
+//standard metrics
+//+ (CGFloat)buttonRadius;
+//+ (CGFloat)buttonDiameter;
++ (CGFloat)buttonRadiusWithModifier:(CGFloat)modifier;
+//+ (CGSize)buttonSize;
+//+ (CGSize)clockButtonInteriorSize; //if the alarm label components were to fit inside a button comfortably, what size would they be?
+//+ (CGFloat)sliderSpacing;
++ (CGFloat)sliderSpacingWithIndex:(int)index;
+
+//animation properties
+//+ (CGFloat)animationDuration;
+//+ (CGFloat)animationDelay;
+
+//colors
++ (UIColor *)backgroundColor;
++ (UIColor *)interfaceColor;
++ (UIColor *)yesColor;
++ (UIColor *)noColor;
++ (UIColor *)transparent;
+
+//time
++ (int)sliderTimeInterval;
++ (int)currentDayNumber;
++ (int)dayNumberFromIdent:(NSString *)ident;
++ (NSDateComponents *)currentDateComponents;
+//+ (NSDate *)sunriseTimeToday; //need to format, submit, recieve and process a web request
++ (NSCalendar *)calendar;
++ (BOOL)isInTheFutureTodayWithHour:(int)hour andMinute:(int)minute;
+
+//user defaults
+
+//local notifications
++ (BOOL)localNotificationExistsWithIdent:(NSString *)localNotificationIdent;
++ (void)postLocalNotificationWithDate:(NSDate *)date andIdent:(NSString *)ident andValue:(CGFloat)value isRecurring:(BOOL)isRecurring;
++ (CGFloat)getValueFromLocalNotificationWithIdent:(NSString *)ident;
++ (void)removeLocalNotificationWithIdent:(NSString *)localNotificationIdent;
+
+//convenience
++ (NSNumber *)i:(int)i;
++ (NSNumber *)f:(CGFloat)f;
++ (BOOL)dictionariesAreEqualWithFirstDictionary:(NSDictionary *)firstDictionary andSecondDictionary:(NSDictionary *)secondDictionary; //test keys and values
+//+ (UIImage *)imageFromView:(UIView *)view;
++ (UIApplication *)app;
+
+//vector methods
++ (CGFloat)getDisplacementOfPointA:(CGPoint)pointA fromPointB:(CGPoint)pointB;
++ (CGFloat)getAngleFromZeroHorizontalWithCenter:(CGPoint)center andPoint:(CGPoint)point;
++ (CGPoint)getOrbitWithCenter:(CGPoint)center andRadius:(CGFloat)radius andAngle:(CGFloat)angle; //specifies point at a certain distance and angle from another
+
 @end
