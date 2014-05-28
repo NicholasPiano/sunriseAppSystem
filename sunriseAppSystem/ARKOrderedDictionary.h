1757c6f1a12265a0dbab4605828b6da4bcfa244a
NicholasPiano <nicholas.d.piano@gmail.com>
2014-05-28 19:55:21 +0100

    Starting ordered dictionary over again because extending NSMutableDictionary requires quite a verbose implementation. Now writing it as simply an NSObject but with a dictionary and an array inside it.

--- sunriseAppSystem/ARKOrderedDictionary.h
+++ sunriseAppSystem/ARKOrderedDictionary.h
@@ -8,22 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-@interface ARKOrderedDictionary : NSMutableDictionary
-
-#pragma mark properties
-@property (strong, nonatomic) NSMutableArray *keyArray;
-
-#pragma mark initialiser
-- (id)init; //setup both dictionary and array
-
-#pragma mark instance methods
-- (void)setObject:(id)object forKey:(id<NSCopying>)key; //default latest index
-- (void)insertObject:(id)object forKey:(id<NSCopying>)key atIndex:(NSUInteger)index;
-- (id)objectAtIndex:(NSUInteger)index;
-- (NSString *)keyAtIndex:(NSUInteger)index;
-- (NSUInteger)indexOfKey:(NSString *)key;
-
-#pragma mark factory
-+ (ARKOrderedDictionary *)dictionary;
+@interface ARKOrderedDictionary : NSObject
 
 @end
